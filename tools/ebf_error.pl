73313718bc758bb8bb4c72d3d4af9cee72bb26b1
Sylwester <sylwester.git@sylwester.no>
2011-05-09 22:14:10 +0000

    fixed error checking script

--- tools/ebf_error.pl
+++ tools/ebf_error.pl
@@ -2,6 +2,7 @@
 
 die("usage: $0 object-file\n") unless defined $ARGV[0];
 die("Compilation of $ARGV[0] failed: zero size\n") if(! -s $ARGV[0] );
+my $file = $ARGV[0];
 $/=undef;
 $in=<>;
-die("compilation of $ARGV[0] failed: $&") if( $in=~ /^.*ERROR.*$/ )
+die("compilation of $file failed: $&\n") if( $in=~ /^.*ERROR.*$/m )
